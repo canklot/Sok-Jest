a0a538ff60b982ec36df7551cb5df3d3732f8100
Canklot <mustafacandan211@gmail.com>
2021-01-25 19:45:35 +0300

    v-for yazıları eklendi

--- store/store.js
+++ store/store.js
@@ -1,12 +1,43 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+export default {
+  state() {
+    return {
+      salad: [
+        "25 Tl ve üzeri alışverişinize SuperFresh Pizza King 780 gr sadece 12 TL ! Sepetiniz 25tl ve üzeri ise ürünü sepete eklediğinizde indirimli fiyat sepetinize otomatik yansır.",
+        "25 Tl ve üzeri alışverişinize Lipton Altın Harman Demlik Poşet 100 lü sadece 14 TL ! Sepetiniz 25tl ve üzeri ise ürünü sepete eklediğinizde indirimli fiyat sepetinize",
+        "Nescafe Gold 150 gr Nestle Çikolata 60 gr sadece 32,45 TL İkili özel paket!",
+        "25 Tl ve üzeri alışverişinize SuperFresh Pizza King 780 gr sadece 12 TL ! Sepetiniz 25tl ve üzeri ise ürünü sepete eklediğinizde indirimli fiyat sepetinize otomatik yansır. ",
+        "25 Tl ve üzeri alışverişinize Ülker Çokokrem Cam Kavanoz 650 gr sadece 14 TL ! Sepetiniz 25tl ve üzeri ise ürünü sepete eklediğinizde indirimli fiyat sepetinize otomatik",
+        "25 Tl ve üzeri alışverişinize Loreal Kolajen Bakımı Günlük Bakım Kremi 50m sadece 25 TL ! Sepetiniz 25tl ve üzeri ise ürünü sepete eklediğinizde indirimli fiyat sepetinize",
+        "25 Tl ve üzeri alışverişinize Loreal Kolajen Bakımı Günlük Bakım Kremi 50m sadece 25 TL ! Sepetiniz 25tl ve üzeri ise ürünü sepete eklediğinizde indirimli fiyat sepetinize",
+        "25 Tl ve üzeri alışverişinize Activex Antibakteriyel Islak Havlu 2x56 Adet sadece 11 TL ! Sepetiniz 25tl ve üzeri ise ürünü sepete eklediğinizde indirimli fiyat sepetinize",
+        "25 Tl ve üzeri alışverişinize Heinz Ketçap 700 Gr Mayonez 400 Gr sadece 19 TL ! Sepetiniz 25tl ve üzeri ise ürünü sepete eklediğinizde indirimli fiyat sepetinize otomatik"
+      ]
+    };
+  },
+
+  mutations: {
+    addIngredient(state, ingredient) {
+      state.salad.push(ingredient);
+    }
+  },
+
+  actions: {
+    addIngredient({ commit }, ingredient) {
+      commit("addIngredient", ingredient);
+    }
+  }
+};
+
+/* Old code */
 /* export const state = () => ({
     kampanya1: '25 Tl ve üzeri alışverişinize SuperFresh Pizza King 780 gr sadece 12 TL ! Sepetiniz 25tl ve üzeri ise ürünü sepete eklediğinizde indirimli fiyat sepetinize otomatik yansır.',
-    kampanya2: ' 25 Tl ve üzeri alışverişinize Lipton Altın Harman Demlik Poşet 100 lü sadece 14 TL ! Sepetiniz 25tl ve üzeri ise ürünü sepete eklediğinizde indirimli fiyat sepetinize',
-    kampanya3:'Nescafe Gold 150 gr Nestle Çikolata 60 gr sadece 32,45 TL İkili özel paket!',
+    kampanya2: '25 Tl ve üzeri alışverişinize Lipton Altın Harman Demlik Poşet 100 lü sadece 14 TL ! Sepetiniz 25tl ve üzeri ise ürünü sepete eklediğinizde indirimli fiyat sepetinize',
+    kampanya3: 'Nescafe Gold 150 gr Nestle Çikolata 60 gr sadece 32,45 TL İkili özel paket!',
     kampanya4: '25 Tl ve üzeri alışverişinize SuperFresh Pizza King 780 gr sadece 12 TL ! Sepetiniz 25tl ve üzeri ise ürünü sepete eklediğinizde indirimli fiyat sepetinize otomatik yansır. ',
     kampanya5: '25 Tl ve üzeri alışverişinize Ülker Çokokrem Cam Kavanoz 650 gr sadece 14 TL ! Sepetiniz 25tl ve üzeri ise ürünü sepete eklediğinizde indirimli fiyat sepetinize otomatik',
     kampanya6: '25 Tl ve üzeri alışverişinize Loreal Kolajen Bakımı Günlük Bakım Kremi 50m sadece 25 TL ! Sepetiniz 25tl ve üzeri ise ürünü sepete eklediğinizde indirimli fiyat sepetinize',
@@ -21,27 +52,3 @@ export const mutations = {
     }
 }
  */
-
-/* ----------------------- */
-
-export default {
-    state() {
-      return {
-        salad: ["asd","awfwaf","aaaa"]
-      }
-    },
-  
-    mutations: {
-      addIngredient(state, ingredient) {
-        state.salad.push(ingredient)
-      }
-    },
-  
-    actions: {
-      addIngredient({ commit }, ingredient) {
-        commit('addIngredient', ingredient)
-      }
-    }
-  }
-  
-  
