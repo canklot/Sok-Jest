a6387485c2aa460d26d2cf37676757b0d2132f9c
Canklot <mustafacandan211@gmail.com>
2021-01-25 19:46:50 +0300

    gereksiz css silindi aynıları kampanya componentinde zaten var

--- components/KampanyalarB.vue
+++ components/KampanyalarB.vue
@@ -40,29 +40,4 @@ export default {
   margin-left: 250px;
   margin-top: 30px;
 }
-.kampanya {
-  margin-right: 30px;
-  height: 240px;
-  width: 364px;
-  float: left;
-  margin-top: 30px;
-}
-.kampanya img {
-  width: 363px;
-  border-radius: 3px;
-  cursor: pointer;
-}
-.metin {
-  width: 363px;
-  font-family: Arial, Helvetica, sans-serif;
-  margin-top: 10px;
-  font-size: 15px;
-  height: 80px;
-}
-.kampanyadetay {
-  font-family: Arial, Helvetica, sans-serif;
-  font-weight: 600;
-  margin-top: 10px;
-  cursor: pointer;
-}
 </style>
